55d301e78601fa4489f1aae184e11d7747ecdc30
adamkjall <adam.kjall@hotmail.com>
2021-01-11 16:02:30 +0100

    redeploy

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,7 @@
 import EmbeddedCheckout from "components/embeddedCheckout";
 
 const Checkout = (props) => {
-  return (
-    <div>
-      <h1 className="text-4xl">I'm the checkout page</h1>
-      <EmbeddedCheckout />
-    </div>
-  );
+  return <EmbeddedCheckout />;
 };
 
 export default Checkout;
