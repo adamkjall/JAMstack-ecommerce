aedf96cd6649403c503f04a99f1a153939d9026e
adamkjall <adam.kjall@hotmail.com>
2021-01-22 13:20:35 +0100

    updated nav links

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -14,32 +14,35 @@ const Navbar = () => {
 
   return (
     <header className="fixed z-50 w-full bg-white shadow-md">
-      <nav className="container mx-auto py-3 px-4 flex justify-between items-center">
+      <nav className="container mx-auto py-3 px-6 grid grid-cols-3 items-center">
         <Link href="/">
           <a className="cursor-pointer">
             <Logo width="50" />
           </a>
         </Link>
-        <div className="flex space-x-8 uppercase">
+        <div className="flex space-x-8 uppercase justify-self-center">
+          <Link href="/products&categoryId=19">
+            <a className="cursor-pointer">Women</a>
+          </Link>
+          <Link href="/products&categoryId=18">
+            <a className="cursor-pointer">Men</a>
+          </Link>
+          {/*  TODO go to sale */}
           <Link href="/products">
-            <a className="cursor-pointer">Products</a>
-          </Link>
-          <Link href="/blog">
-            <a className="cursor-pointer">Blog</a>
-          </Link>
-          <Link href="/about">
-            <a className="cursor-pointer">About</a>
+            <a className="cursor-pointer">Sale</a>
           </Link>
         </div>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 justify-self-end">
           <Searchbar />
-          <HeartIcon width="24" className="cursor-pointer" />
-          <UserIcon width="24" className="cursor-pointer" />
-          <CartIcon
-            width="24"
-            onClick={openSidebar}
-            className="cursor-pointer"
-          />
+          <div className="flex align-center space-x-4">
+            <HeartIcon width="24" className="cursor-pointer" />
+            <UserIcon width="24" className="cursor-pointer" />
+            <CartIcon
+              width="24"
+              onClick={openSidebar}
+              className="cursor-pointer"
+            />
+          </div>
         </div>
       </nav>
     </header>
