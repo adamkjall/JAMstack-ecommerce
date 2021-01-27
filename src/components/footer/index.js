0ba3b4bf7e5643d0fbda842879662eac18d697d4
adamkjall <adam.kjall@hotmail.com>
2021-01-27 16:06:58 +0100

    added padding to footer so to match navbar

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer id="footer" className="bg-black">
       {/* <!-- start container --> */}
-      <div className="container mx-auto pt-8 pb-4">
+      <div className="container mx-auto px-4 pt-8 pb-4">
         <div className="flex flex-wrap overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
           <div className="w-full overflow-hidden sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
             {/* <!-- Column 1 Content --> */}
